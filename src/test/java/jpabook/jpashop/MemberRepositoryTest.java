a26b4576e6f6771914be0a3c0950ebe680897a49
seonghuncode <gnos456@naver.com>
2023-12-30 02:19:28 +0900

    쿼리 파라미터 콘솔에 나오게 하는 설정

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import javax.transaction.Transactional;
@@ -24,6 +25,7 @@ class MemberRepositoryTest {
     @Test
     @Transactional //어노테이션 사용 이유 : 엔티티의 변경은 모두 트랜잭션 안에서 이루어 져야 한다.
     // (해당 어노테이션이 Test에 있으면 실행하고 바로 Rollback을 하기 때문에 DB에 반영되지 않는다. / @Rollvack(false)를 통해 무력화 가능)
+    @Rollback(false)
     public void testMember() throws Exception{
 
         //given
@@ -38,7 +40,7 @@ class MemberRepositoryTest {
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
         assertThat(findMember).isEqualTo(member); //True : 같은 트핸잭션 안에서 저장, 조회시 사용하는 영속성 컨텍스트가 동일하다
-        // -> 동일한 영속성 컨텍스트에서 같은 키값으로 조회하기 때문에 일치
-    }
+        // -> 동일한 영속성 컨텍스트에서 같은 키값으로 조회하기 때문에 일치치
+        }
 
 }

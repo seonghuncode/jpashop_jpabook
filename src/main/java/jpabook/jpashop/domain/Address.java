0328b3be8d4b5aa18c1aaa4f4b9da7085d159b27
seonghuncode <gnos456@naver.com>
2024-01-03 22:42:42 +0900

    엔티티 클래스 계발

--- src/main/java/jpabook/jpashop/domain/Address.java
+++ src/main/java/jpabook/jpashop/domain/Address.java
@@ -10,4 +10,14 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    protected Address() {
+    }
+    //값 타입의 경우 변경이 불가능 해야 하기 때문에 Getter만 허용하고 생성자를 통해 초기 에만 만들어지도록 한다.
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
+
 }
---
+++ src/main/java/jpabook/jpashop/domain/item/Category.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.domain.item;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Category {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "category_id")
+    private Long id;
+
+    @ManyToMany
+    @JoinTable(name = "category_item",
+        joinColumns = @JoinColumn(name = "category_id"), //.중간 테이블에 있는 id
+            inverseJoinColumns = @JoinColumn(name = "item_id")
+    )
+    // 객체는 컬렉션이 있기 때문에 다대다 매핑이 가능하지만, 관계형 데이터베이스는 다대다 매핑이 불가능 하기 때문에 일대다/다대일로 변환하는 중간 테이블 필요
+    //실무에서는 거의 사용하지 않는다. (필드 추가가 불가능 하기 때문)
+    private List<Item> items = new ArrayList<>();
+
+    @ManyToOne
+    @JoinColumn(name = "parent_id")
+    private Category parent;
+
+    @OneToMany(mappedBy = "parent")
+    private List<Category> child = new ArrayList<>();
+
+}

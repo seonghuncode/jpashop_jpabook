4f388ca16911cad7d8cd0c7e3bf6a187d98e0418
seonghuncode <gnos456@naver.com>
2024-01-10 16:44:15 +0900

    주문 서비스 갭라

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,35 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    /*
+    클래스에 Transaction은 readOnly이기 때문에 수정이 불가능 하다.
+    메서드 위에 Transactional 어노테이션을 사용하면 메서드에 붙어 있는것이 우선권이 높기 때문에 수정 가능
+     */
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
+}

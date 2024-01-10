4579d520299081d36862b2b387fbe33c819a6f0b
seonghuncode <gnos456@naver.com>
2024-01-10 16:14:42 +0900

    상품 엔티티 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.domain.item;
 
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -25,4 +26,28 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+
+    /*
+     * 비즈니스 로직 작성 (재고 증감 로직)
+     * 엔티티 자체에서 해결할 수 있는 로직은 엔티티 안에 개발 하는 것이 좋다.
+     *
+     * 데이터를 가지고 있는쪽에 비즈니스 메서드를 가지고 있는 것이 유리
+     * 외부에서 Setter를 통해 값을 변경하는 것보다 재고 증감 메서드 같이 특정 메서드를 통해 값을 변경하는 것이 객체지향적이다
+     */
+    //제고 증가
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    //재고 감소
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 }
--- src/test/java/jpabook/jpashop/service/MemberServiceTest.java
+++ src/test/java/jpabook/jpashop/service/MemberServiceTest.java
@@ -6,12 +6,8 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
 
 import static org.junit.jupiter.api.Assertions.*;
 

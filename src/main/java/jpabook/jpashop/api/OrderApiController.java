b4b22fa821bf39a00b5bb22eb25337f42dd2bc67
seonghuncode <gnos456@naver.com>
2024-02-06 22:20:38 +0900

    일대다 관계에서 엔티티를 DTO로 변환해서 반환하는 API개발(최적화 필요)

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -1,14 +1,20 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,6 +22,7 @@ public class OrderApiController {
 
     private final OrderRepository orderRepository;
 
+    //엔티티를 직접 노출하는 방법
     @GetMapping("/api/v1/orders")
     public List<Order> orderV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
@@ -32,4 +39,64 @@ public class OrderApiController {
         }
         return all;
     }
+
+
+
+
+    //엔티티를 DTO로 변환해서 반환하는 방법
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2(){
+        //엔티티 조회
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+
+        //orders -> DTO로 변환
+        List<OrderDto> result = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+
+        return result;
+    }
+
+    @Getter
+    static class OrderDto{
+
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate  = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            //orderItems는 엔티티이기 때문에 DTO로 변환 시켜서 반환해주어야 한다.
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+
+    }
+
+
+    //DTO안에 있는 엔티티에 대해 DTO로 변환할 DTO클래스
+    @Getter
+    static class OrderItemDto{
+
+        private String itemName; //상품명
+        private int orderPrice; //주문가격
+        private int count; //주문수량
+
+        public OrderItemDto(OrderItem orderItem){
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+
+
+
 }
